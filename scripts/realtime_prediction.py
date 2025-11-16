eb3c82825fccd90c8d5d16bc19af6941e4d96b12
FatimaZafarrr <fatima.zafarrr99@gmail.com>
2025-11-17 02:37:53 +0500

    Update realtime_prediction.py

--- scripts/realtime_prediction.py
+++ scripts/realtime_prediction.py
@@ -4,22 +4,19 @@ import numpy as np
 from tensorflow.keras.models import load_model
 import json
 
-# === PATHS ===
 MODEL_PATH = r"C:\Users\Hp\Desktop\HandGesture\hand_gesture_model.h5"
 LABELS_PATH = r"C:\Users\Hp\Desktop\HandGesture\gesture_labels.json"
 
-# === LOAD MODEL AND LABELS ===
 model = load_model(MODEL_PATH)
 with open(LABELS_PATH, 'r') as f:
     class_indices = json.load(f)
 inv_class_indices = {v: k for k, v in class_indices.items()}
 
-# === MEDIAPIPE HANDS ===
 mp_hands = mp.solutions.hands
 mp_draw = mp.solutions.drawing_utils
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 
-# === START WEBCAM ===
+
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -34,14 +31,14 @@ while True:
         for hand_landmarks in result.multi_hand_landmarks:
             mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-            # Get bounding box of hand
+           
             h, w, _ = frame.shape
             x_min = int(min([lm.x for lm in hand_landmarks.landmark]) * w) - 10
             x_max = int(max([lm.x for lm in hand_landmarks.landmark]) * w) + 10
             y_min = int(min([lm.y for lm in hand_landmarks.landmark]) * h) - 10
             y_max = int(max([lm.y for lm in hand_landmarks.landmark]) * h) + 10
 
-            # Crop and preprocess hand image
+        
             hand_img = frame[y_min:y_max, x_min:x_max]
             if hand_img.size == 0:
                 continue
